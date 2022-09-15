76ac4b1a053fa27d1cc9eee2c5cc7fd1c08e2b78
Ana clara <dev.anaclaramota@outlook.com>
2022-09-14 22:17:49 -0300

    ex-06

---
+++ fun.js
@@ -0,0 +1,19 @@
+/*let nome = 'Paulo'
+function ola (){
+    console.log ('ola ' + nome);
+}
+ola()
+function ola (nomeUsuario){
+    console.log ('ola ' + nomeUsuario);
+}
+ola('Maria')*/
+
+
+
+function calc(anoNasc){
+    let idade = 2022 - anoNasc
+    return idade
+}
+calc(2000)
+
+
